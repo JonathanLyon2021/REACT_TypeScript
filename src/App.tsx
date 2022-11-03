db8ee59d1e273c7e1f45bc279aa4d27286ad73a2
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-11-03 03:35:12 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 
-
+//Conventional Props
+//list of the different properties you'vegot then {title: string} the different data types that you have associated with those properties.
 function Heading({ title }: { title: string }) {
   return (
     <h1>{title}</h1>
   );
 }
 
-function HeadingWithContent({ children }: { children: ReactNode }): ReactElement { //ReactElement somehow helps define this??? Ask Jason
+//children should be a ReactNode. Checkout define ReactNode, you can see the different types there
+function HeadingWithContent({ children }: { children: ReactNode }): ReactElement { 
   return (
     <h1>{children}</h1>
   );
